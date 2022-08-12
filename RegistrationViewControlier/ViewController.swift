1e6f3b82a3965674d1df6a47aa41ae2fc464504b
Albina <olifirenko.albina@gmail.com>
2022-08-12 11:15:58 +0300

    Initial Commit

---
+++ RegistrationViewControlier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistrationViewControlier
+//
+//  Created by mac on 8/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
